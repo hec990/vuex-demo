59b11f5c8b1d79df5c4aceb0cb62c296ada6273c
xiaohh <1296522554@qq.com>
2021-11-02 09:48:48 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Addition />
+    -----------------------------
+    <Subtraction />
+  </div>
+</template>
+
+<script lang="js">
+import Addition from './components/Addition'
+import Subtraction from './components/Subtraction'
+ export default {
+   name:"app",
+   components:{
+     Addition,
+     Subtraction
+   }
+ }
+</script>
---
+++ src/components/Addition.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>当前最新的count值为：</h3>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "addition"
+}
+</script>
