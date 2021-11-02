16db4be1693b09ffdbb459a662c06ce560d13ee8
xiaohh <1296522554@qq.com>
2021-11-02 10:50:19 +0800

    简写方法

--- src/components/Subtraction.vue
+++ src/components/Subtraction.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-<!--    <h3>当前最新的count值为：{{count}}</h3>-->
-<!--    <h3>{{$store.getters.showNum}}</h3>-->
     <h3>{{showNum}}</h3>
-    <button @click="sub1"> -1 </button>
-    <button @click="sub2"> -N </button>
-    <button @click="sub3">1s后-1</button>
-    <button @click="sub4">1s后-N</button>
+    <button @click="sub"> -1 </button>
+    <button @click="subN(10)"> -N </button>
+    <button @click="subAsync">1s后-1</button>
+    <button @click="subNAsync(100)">1s后-N</button>
   </div>
 </template>
 
@@ -21,18 +19,6 @@ export default {
   methods:{
     ...mapMutations(['sub','subN']),
     ...mapActions(['subAsync','subNAsync']),
-    sub1(){
-      this.sub()
-    },
-    sub2(){
-      this.subN(10)
-    },
-    sub3(){
-      this.subAsync()
-    },
-    sub4(){
-      this.subNAsync(100)
-    },
   }
 }
 </script>
